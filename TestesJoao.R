1b0f269432b337d835dc28208aba8d7cf209ab62
iagocam <33329167+iagocam@users.noreply.github.com>
2017-11-07 22:22:30 -0200

    Update TestesJoao.R

--- TestesJoao.R
+++ TestesJoao.R
@@ -71,3 +71,47 @@ plot4.orcamento.premio <- ggplot(filmesIMDB, aes(filmeBudget, imdbRating, col = 
 plot4.orcamento.premio
 plot4.orcamento.premio.separado <- plot4.orcamento.premio + facet_grid(~as.factor(unlist(Awards)),5)
 plot4.orcamento.premio.separado
+
+
+
+
+########################################  João, nao quero cagar o que tu já fez, entao to separando s2 bjo na bunda  ############################################################################
+# testes envolvendo bilheteria
+## correlação entre ano e bilheteria
+plot1 <- ggplot(filmesIMDB, aes(x = Year, y = filmeDomGross, col = as.factor(unlist(Genre)))) + geom_point()
+plot1
+
+plot1.genero <- plot1 + facet_wrap(~as.factor(unlist(Genre)), 5)
+plot1.genero + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre ano e bilheteria") + labs(y = "Bilheteria", x = "Ano")
+
+## correlação entre orçamento e bilheteria
+
+plot2 <- ggplot(filmesIMDB, aes(filmeBudget, filmeDomGross)) + geom_point()
+plot2 + geom_smooth() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre orçamento e bilheteria") + labs(y = "Bilheteria", x = "Orçamento")
+
+plot2.premios <- ggplot(filmesIMDB, aes(filmeBudget, filmeDomGross, col = as.factor(unlist(Awards)))) + geom_point() #premios
+plot2.premios + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre orçamento e bilheteria") + labs(y = "Bilheteria", x = "Orçamento")
+
+plot2.premios2 <- plot2.premios + facet_wrap(~as.factor(unlist(Awards)), 5)
+plot2.premios2 + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre orçamento e bilheteria") + labs(y = "Bilheteria", x = "Orçamento")
+
+plot2.bp.premios <- ggplot(filmesIMDB, aes(as.factor(unlist(Awards)),filmeDomGross)) + geom_boxplot() + facet_wrap(~as.factor(unlist(Awards)), 5)
+plot2.bp.premios + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre orçamento e bilheteria") + labs(y = "Bilheteria", x = "Orçamento")
+
+## correlação entre notas do IMDB e bilheteria
+plot3 <- ggplot(filmesIMDB, aes(imdbRating, filmeDomGross)) + geom_point()
+plot3 + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre notas do IMDB e bilheteria") + labs(y = "Bilheteria", x = "Nota IMDB")
+
+plot3.genero <- ggplot(filmesIMDB, aes(imdbRating, filmeDomGross, col = as.factor(unlist(Genre)) )) + geom_point()
+plot3.genero + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre notas do IMDB e bilheteria") + labs(y = "Bilheteria", x = "Nota IMDB")
+
+plot3.genero.separado <- plot3.genero + facet_wrap(~as.factor(unlist(Genre)), 5)
+plot3.genero.separado + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre notas do IMDB e bilheteria") + labs(y = "Bilheteria", x = "Nota IMDB")
+
+plot3.premios <- ggplot(filmesIMDB, aes(imdbRating, filmeDomGross, col = as.factor(unlist(Awards)))) + geom_point() #premios
+plot3.premios + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre notas do IMDB e bilheteria") + labs(y = "Bilheteria", x = "Nota IMDB")
+
+plot3.premios.separado <- plot3.premios + facet_wrap(~as.factor(unlist(Awards)), 5)
+plot3.premios.separado + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Correlação entre notas do IMDB e bilheteria") + labs(y = "Bilheteria", x = "Nota IMDB")
+
+###################################################################################################################
